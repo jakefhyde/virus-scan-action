df4c45d8956dde63d7905f24781459ae75242dc6
Ricardo Weir <ricardo.weir@suse.com>
2021-08-30 18:32:26 -0700

    Test scan results

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
 clamscan -r > output.txt
-cat output.txt | grep "Infected"
+a=$(cat output.txt | grep "Infected")
+read -a ARR <<< $(cat output.txt | grep "Infected")
+if [ ${ARR[2]} != 0 ]; then
+  exit 1
+else
+  echo "success"
+fi
