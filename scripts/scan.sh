8452d81d6869ffc3118d21ba02113ced7d57a7f9
Ricardo Weir <ricardo.weir@suse.com>
2021-09-14 11:12:42 -0700

    Support multi scanning

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+MODE=$1
+IMAGES_FILENAME=$2
+if [ ${MODE} = "multi" ]; then
+  IFS=$'\n' read -a IMAGES <<< $(cat $IMAGES_FILENAME)
+  mkdir images_scan
+  echo ${IMAGES}
+  for image in ${IMAGES}
+  do
+    docker pull ${image}
+    sudo docker save ${image} > images_scan/$(tr '/' '-' <<< ${image}.tar)
+  done
+  cd images_scan
+elif [ ${MODE} != "single" ]; then
+  echo "Invalid mode: ${MODE}"
+  exit 1
+fi
 clamscan -r > output.txt
 cat output.txt
 read -a ARR <<< $(cat output.txt | grep "Infected")
